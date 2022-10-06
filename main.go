d31f13f25bdb49abf053516594b0313c44583d9b
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-06 15:25:32 +0700

    init

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,21 +11,22 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	//path dengan variable id
 	router.GET("/books/:id", booksHandler)
-
 	//path dengan variable id dan title
 	router.GET("/books/:id/:title", booksHandlers)
-
 	//membuat request query untuk id
 	router.GET("/query", queryHandler)
-
 	//membuat multi request query title dan price
 	router.GET("/queries", queryHandlers)
 
+	//Post request
+	router.POST("/books", postBookHandler)
+
 	router.Run()
 }
+
+/*GET*/
 
 //function handler
 func rootHandler(c *gin.Context) {
@@ -38,6 +40,7 @@ func rootHandler(c *gin.Context) {
 func booksHandler(c *gin.Context){
 	//mengambil parameter id
 	id := c.Param("id")
+	//mengembalikan nilai json
 	c.JSON(http.StatusOK, gin.H{"id": id})
 }
 
@@ -45,7 +48,7 @@ func booksHandlers(c *gin.Context){
 	//mengambil parameter id dan title
 	id := c.Param("id")
 	title := c.Param("title")
-
+	//mengembalikan nilai json
 	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
 }
 
@@ -53,6 +56,7 @@ func booksHandlers(c *gin.Context){
 func queryHandler(c *gin.Context){
 	//mengambil query id
 	id := c.Query("id")
+	//mengembalikan nilai json
 	c.JSON(http.StatusOK, gin.H{"id": id})
 }
 
@@ -61,5 +65,27 @@ func queryHandlers(c *gin.Context){
 	//mengambil query title dan price
 	title := c.Query("title")
 	price := c.Query("price")
+	
+	//mengembalikan nilai json
 	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+/*POST*/
+
+//membuat struct untuk menangkap data post request
+type BookInput struct{
+	Title string
+	Price int
+}
+//function handler query untuk post
+func postBookHandler(c *gin.Context){
+	//membuat variable input
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil{
+		log.Fatal(err)
+	}
+	//mengembalikan nilai json
+	c.JSON(http.StatusOK, gin.H{"title": bookInput.Title, "price": bookInput.Price})
 }
