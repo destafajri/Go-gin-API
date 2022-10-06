b4fd13c3fd70f3232a76f4990a129143fa9b1355
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-06 20:55:34 +0700

    delete db

--- main.go
+++ main.go
@@ -27,16 +27,16 @@ func main() {
 
 	/*****CRUD database***/
 	/*Create*/
-	// book := books.Book{}
-	// book.Title = "Ekoju"
-	// book.Price = 50000
-	// book.Discount = 100
-	// book.Rating = 10
-	// book.Description = "Tips cuan dari Eko alias Lord Oura nih"
-	// er := db.Debug().Create(&book).Error
-	// if er != nil{
-	// 	log.Fatal("Create request error")
-	// }
+	book := books.Book{}
+	book.Title = "Ekoju"
+	book.Price = 50000
+	book.Discount = 100
+	book.Rating = 10
+	book.Description = "Tips cuan dari Eko alias Lord Oura nih"
+	er := db.Debug().Create(&book).Error
+	if er != nil{
+		log.Fatal("Create request error")
+	}
 
 	/*Read*/
 	//read first book
@@ -65,27 +65,52 @@ func main() {
 	fmt.Printf("Books object ===>%v ", book3)
 
 	//read all object
-	var books []books.Book
-	errs := db.Debug().Find(&books).Error
-	if errs != nil{
-		log.Fatal("read request error")
-	}
-	for _, p := range books{
-		fmt.Println("Books Title ", p.Title)
-		fmt.Printf("Books object ===> %v ", p)
-	}
+	// var books []books.Book
+	// errs := db.Debug().Find(&books).Error
+	// if errs != nil{
+	// 	log.Fatal("read request error")
+	// }
+	// for _, p := range books{
+	// 	fmt.Println("Books Title ", p.Title)
+	// 	fmt.Printf("Books object ===> %v ", p)
+	// }
 
 	//read book with condition
 	// var books2 []books.Book
-	errs2 := db.Debug().Where("price > 500000").Find(&books).Error
-	if errs2 != nil{
+	// errs2 := db.Debug().Where("price > 500000").Find(&books).Error
+	// if errs2 != nil{
+	// 	log.Fatal("read request error")
+	// }
+	// for _, p := range books{
+	// 	fmt.Println("Books Title ", p.Title)
+	// 	fmt.Printf("Books object ===> %v ", p)
+	// }
+	
+	/*Update*/
+	//select the data
+	var uBook books.Book
+	errU := db.Debug().First(&uBook, 5).Error
+	if errU != nil{
 		log.Fatal("read request error")
 	}
-	for _, p := range books{
-		fmt.Println("Books Title ", p.Title)
-		fmt.Printf("Books object ===> %v ", p)
+	fmt.Println("Books Title ", uBook.Title)
+	fmt.Printf("Books object ===> %v ", uBook)
+	//and then update
+	uBook.Title = "Eko Up to date lagi nih"
+	db.Debug().Save(&uBook)
+	fmt.Println("Books Update ", uBook)
+
+	/*Delete*/
+	//Select the data
+	var dbook books.Book
+	errd := db.Debug().Last(&dbook).Error
+	if errd != nil{
+		log.Fatal("read request error")
 	}
-	
+	//And the delete
+	db.Debug().Delete(&dbook)
+
+
 /*********API Request**********/
 	//router default setting
 	router := gin.Default()
